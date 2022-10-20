edb25070261f35bb6a9920af633cfcc17a9b44be
mateu <mateu@10.33.2.19>
2022-10-20 16:39:16 -0300

    Semana 4 - conclusão Crud vaga

--- estacionamento/src/connection/script.sql
+++ estacionamento/src/connection/script.sql
@@ -7,11 +7,11 @@
  * Created: 6 de out. de 2022
  */
 
-CREAT DATABASE IF NOT EXISTS estacionamento;
+CREATE DATABASE IF NOT EXISTS estacionamento;
 USE estacionamento;
 CREATE TABLE vaga (
-idVaga int NOT NULL ALTO_INCREMENT,
+idVaga int NOT NULL AUTO_INCREMENT,
 numero int NOT NULL,
 rua VARCHAR(100) NOT NULL,
-obliqua booleam NOT NULL,
+obliqua boolean NOT NULL,
 PRIMARY KEY (idVaga));
